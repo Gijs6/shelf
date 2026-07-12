02b86e8049394af1031896ed4924cea18295df0e
Gijs <gijstenberg@gmail.com>
2026-07-12 22:20:54 +0200

    Fancy markdown shizzle

---
+++ static/scripts/markdown-editor.js
@@ -0,0 +1,16 @@
+(function () {
+    document.querySelectorAll("textarea.editor__content").forEach(function (textarea) {
+        var mde = new EasyMDE({
+            element: textarea,
+            toolbar: false,
+            status: false,
+            spellChecker: false,
+            autoDownloadFontAwesome: false,
+            placeholder: textarea.getAttribute("placeholder") || "",
+        });
+
+        mde.codemirror.on("change", function () {
+            textarea.dispatchEvent(new Event("input", { bubbles: true }));
+        });
+    });
+})();
