e3f338a2a0df5e6f8b23a4812001ae58364a09fa
Gijs <gijstenberg@gmail.com>
2026-07-21 11:21:34 +0200

    If search is empty, just return everything

--- routes/search.py
+++ routes/search.py
@@ -9,5 +9,5 @@ search_bp = Blueprint("search", __name__, url_prefix="/search")
 @search_bp.get("/")
 def search_view():
     query = request.args.get("q", "").strip()
-    results, parsed = search(query) if query else ([], None)
+    results, parsed = search(query)
     return render_template("search.jinja", query=query, results=results, parsed=parsed)
